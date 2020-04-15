f811c0c974394f5480713d5ea307dc724adf3e04
NishizawaShota <nishizawa@emol.jp>
2020-04-15 17:43:27 +0900

    Initial Commit

---
+++ SwiftUIViewTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIViewTest
+//
+//  Created by Shota Nishizawa on 2020/04/15.
+//  Copyright © 2020 Shota Nishizawa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
